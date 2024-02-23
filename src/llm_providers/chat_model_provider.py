10e17812b308cae25490ea8a59e8ac9ca2f851db
Liang521Bo <jialiang6699@gmail.com>
2024-02-23 16:12:23 +0100

    add FASTAPI_ENVIRONMENT variable check

--- src/llm_providers/chat_model_provider.py
+++ src/llm_providers/chat_model_provider.py
@@ -2,7 +2,7 @@ from typing import Union
 from langchain_community.chat_models import ChatOllama
 from langchain_openai import AzureChatOpenAI
 from config import Settings
-
+import os
 
 
 class ChatModelProvider:
@@ -11,7 +11,10 @@ class ChatModelProvider:
         model_name: str, **model_kwargs
     ) -> Union[ChatOllama, AzureChatOpenAI]:
         if model_name in ["GPT-Turbo", "GPT-4", "GPT-35-turbo-16k", "GPT-4-Turbo"]:
-            return AzureChatOpenAI(**model_kwargs, deployment_name=model_name)
+            if os.environ["FASTAPI_ENVIRONMENT"] == "dev":
+                return AzureChatOpenAI(**model_kwargs, deployment_name="GPT-Turbo")
+            else:
+                return AzureChatOpenAI(**model_kwargs, deployment_name=model_name)
         else:
             raise ValueError(f"Model {model_name} not supported")
 
@@ -24,5 +27,9 @@ azure_model_args = {
     "openai_api_type": settings.api_type,
     "temperature": 0.0,
 }
-llm = ChatModelProvider.get_chat_model("GPT-Turbo", **azure_model_args)
-llm4 = ChatModelProvider.get_chat_model("GPT-4", **azure_model_args)
+if os.environ["FASTAPI_ENVIRONMENT"] == "dev":
+    llm = ChatModelProvider.get_chat_model("GPT-Turbo", **azure_model_args)
+    llm4 = ChatModelProvider.get_chat_model("GPT-4", **azure_model_args)
+else:
+    llm = ChatModelProvider.get_chat_model("GPT-Turbo", **azure_model_args)
+    llm4 = ChatModelProvider.get_chat_model("GPT-4", **azure_model_args)
